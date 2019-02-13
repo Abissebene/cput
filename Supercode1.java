1cb5f4d9eafd7f3b23e8e1190b00a37112995969
Abissebene <47355998+Abissebene@users.noreply.github.com>
2019-02-13 12:37:36 +0200

    Add files via upload

---
+++ Supercode1.java
@@ -0,0 +1,18 @@
+public class Supercode1
+    {
+        public static void main(String[] agrs){
+            int i = 0;
+            while (true)//setting up the condition
+            {
+                i++;
+                System.out.println(i);
+
+                if (i != 20)// the program is suposed to print all the number and then break after 20
+                {
+                    break;
+                }//end of if
+
+            }//end of while loop
+
+        }
+    }//end of the class
